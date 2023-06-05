6e50abcc752257b20ee24bfc25d97b6cf9d33371
kuanchiliao1 <kuanchiliao@gmail.com>
2023-06-05 16:18:48 -0700

    Modify to work with multiple menus

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const dropdownBtn = document.querySelector('.dropdown-btn');
+const dropdownBtns = document.querySelectorAll('.dropdown-btn');
 
-dropdownBtn.addEventListener('click', function() {
-  const container = document.querySelector('.dropdown-option-container');
-  container.classList.toggle('expanded')
-  dropdownBtn.setAttribute('aria-expanded',
-    !(dropdownBtn.getAttribute('aria-expanded') === "true"))
-    console.log(!dropdownBtn.getAttribute('aria-expanded'))
+dropdownBtns.forEach(dropdownBtn => {
+  dropdownBtn.addEventListener('click', function() {
+    // const container = document.querySelector('.dropdown-option-container');
+    const container = dropdownBtn.nextElementSibling
+    container.classList.toggle('expanded')
+    dropdownBtn.setAttribute('aria-expanded',
+      !(dropdownBtn.getAttribute('aria-expanded') === "true"))
+      console.log(!dropdownBtn.getAttribute('aria-expanded'))
+  })
 })
