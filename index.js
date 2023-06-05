3e9455c36120e6baf3bb689956152cc6ee1001df
kuanchiliao1 <kuanchiliao@gmail.com>
2023-06-05 15:44:53 -0700

    Add logic for toggling aria

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ const dropdownBtn = document.querySelector('.dropdown-btn');
 dropdownBtn.addEventListener('click', function() {
   const container = document.querySelector('.dropdown-option-container');
   container.classList.toggle('expanded')
+  dropdownBtn.setAttribute('aria-expanded',
+    !(dropdownBtn.getAttribute('aria-expanded') === "true"))
+    console.log(!dropdownBtn.getAttribute('aria-expanded'))
 })
